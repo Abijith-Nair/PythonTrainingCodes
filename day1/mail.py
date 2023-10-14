4e184c91a36c8c833d29803f1d0a3998de0dfb49
Abijith Nair <109901609+Abijith-Nair@users.noreply.github.com>
2023-10-14 07:45:17 +0530

    Delete day1 directory

--- day1/mail.py
+++
@@ -1,7 +0,0 @@
-import re
-text=input("Enter an email")
-pattern="^(?!.*@(yahoo|hotmail)\.(com|in|org)$).*\S+@\S+\.(com|in|org)$"
-if re.findall(pattern,text)!=[]:
-    print("The email is Valid")
-else:
-    print("Invalid email")
--- day1/prg2.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Enter a number to generate a multiplication table: "))
-
-print("Multiplication table for",number,":")
-
-for i in range(0, 10):
-    prod = number * (i+1)
-    print(number,"x",i+1,"=",prod)
